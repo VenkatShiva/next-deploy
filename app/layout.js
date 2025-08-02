075008d4a73054f57a7137b9a5e089a3d7dac340
VenkatShiva <avenkatashiva@gmail.com>
2025-08-02 20:55:20 +0530

    without images

---
+++ app/layout.js
@@ -0,0 +1,45 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import { Overlock } from "next/font/google";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+const overlock = Overlock({
+  subsets: ["latin"],
+  variable: "--font-overlock", // Optional: for using it in CSS
+  weight: ["400", "700"],      // Optional: Regular and Bold
+});
+
+const inter = Inter({
+  variable: "--font-inter",
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"], // Optional: Regular, Medium, SemiBold, Bold
+});
+
+export const metadata = {
+  title: "Prana Healthcare - Primary & Urgent Care Services",
+  description: "Ad Vitam - For Life",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        <Header />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
+}
