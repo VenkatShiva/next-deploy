13b2aa075bed45b72c844895a3a33e6221e5c323
VenkatShiva <avenkatashiva@gmail.com>
2025-08-03 08:39:09 +0530

    changes added

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,15 @@
 /** @type {import('next').NextConfig} */
 
-const isProd = process.env.NODE_ENV === "production";
+// const isProd = process.env.NODE_ENV === "production";
+
+const isProd =  true;
 
 const nextConfig = {
   output: "export",
   reactStrictMode: true,
+  basePath: isProd ? "/next-deploy" : "",
+  trailingSlash: true,
+  assetPrefix: isProd ? "/next-deploy/" : "",
   images: {
     unoptimized: true,
   },
